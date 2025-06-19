6e3351a42fb21fa2062e565cd9e3e9113a580491
giovana-rosa <giovana.rosaifc@gmail.com>
2025-06-19 12:09:24 -0300

    FEAT: campo data de nascimento adicionado no cadastro aluno.

--- gestao_escolar/models.py
+++ gestao_escolar/models.py
@@ -12,7 +12,7 @@ class Professor(models.Model):
     data_de_nascimento = models.DateField()
     formacao = models.CharField(max_length=50, verbose_name='Formação')
     matricula = models.CharField(max_length=20)
-    email = models.CharField(max_length=50, default='admin@admin.com')
+    email = models.CharField(max_length=50)
 
     def __str__(self):
         return self.nome
@@ -28,6 +28,9 @@ class Frequencia(models.Model):
 
 class Aluno(models.Model):
     nome = models.CharField(max_length=50)
+    CPF = models.CharField(max_length=11)
+    data_de_nascimento = models.DateField()
+
    
 
     def __str__(self):
@@ -54,8 +57,8 @@ class Curso(models.Model):
     nome = models.CharField(max_length=50)  
     ementa = models.CharField(max_length=2000)  
     carga_horaria = models.DecimalField(max_digits=5, decimal_places=2, verbose_name='Carga Horária') 
-    coordenador = models.CharField(max_length=50, default='Coordenador Padrão')  
-    email = models.EmailField(max_length=50, default='admin@admin.com')  
+    coordenador = models.CharField(max_length=50)  
+    email = models.EmailField(max_length=50)  
 
     def __str__(self):
         return self.nome
